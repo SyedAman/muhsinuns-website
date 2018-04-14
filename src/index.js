90aff959ee70bda44a03e2b65540e9ce39d20ea2
Syed Aman <sna274@nyu.edu>
2018-04-14 16:51:45 -0500

    make soem changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
+// // Make an array.
+// const foods = ["Chocolate milkshake", "Apples", "Banana smoothie"];
+
+// // Loop through the array using a for loop and log every food.
+// for (const foods = 6; foods == 'Banana smoothie') {
+//   console.log(foods);
+// }
+
+const test = "Hi";
+
+for (let count = 0; count === 10; count++) {
+  console.log(test);
+}
+
 document.getElementById("app").innerHTML = `
 <h1>Portfolio</h1>
-<div>
+div>
   Look
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>
   for more info about Parcel.
