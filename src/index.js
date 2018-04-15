08fac22ec1a3143876aa89b5827b3d9d9e039886
Muhsinun Chowdhury <muhsinun08@gmail.com>
2018-04-15 15:25:57 -0400

    Fix div tag

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@
 
 document.getElementById("app").innerHTML = `
 <h1>Portfolio</h1>
-div>
+<div>
   Look
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>
   for more info about Parcel.
